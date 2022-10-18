e2e6d6da9c9ab5f4a7804168dea287175cc77e46
shivaraj <shivarajwaddar123@gmail.com>
2022-10-18 14:57:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div id="App">
+      <div id="wrapper">
+        <div id="textdiv">
+          <h2>Reliable, efficient deliver</h2>
+          <h2>Powered by Technology</h2>
+          <p>it is simply dummy text printing and typesetting industry</p>
+        </div>
+        <div id="boxes">
+          <div className="col">
+            <div id="box1" >
+              <h3>Supervisor</h3>
+              <p>loraum lipsoma simply dummy </p>
+              <p>text of printing</p>
+            </div>
+          </div>
+          <div className="col" >
+            <div id="box2">
+              <h3>Team builder</h3>
+              <p>loraum lipsoma simply dummy </p>
+              <p>text of printing</p>
+            </div>
+            <div id="box3">
+              <h3>karma</h3>
+              <p>loraum lipsoma simply dummy </p>
+              <p>text of printing</p>
+            </div>
+          </div>
+          <div className="col" >
+            <div id="box4" >
+              <h3>calculator</h3>
+              <p>loraum lipsoma simply dummy </p>
+              <p>text of printing</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
